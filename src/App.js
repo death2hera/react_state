15cbe0cdb518aef4b6afaf46c76eb754ee38a47e
Watti Favour <favourwatti13@gmail.com>
2023-03-15 09:23:11 +0100

    react states

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+class App extends Component{
+  constructor(props){
+    super(props)
+    this.state={show: true, fullname: "john doe", pishur: "john doe.png", profession:"fullstack developer"}
+    this.togglediv=this.togglediv.bind(this)
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  }
+  togglediv=()=>{
+    const { show }=this.state;
+    this.setState({ show: !show})
+  }
+  render(){
+    return(
+      <>
+      <div className='App'>
+        <h1>
+          {this.state.fullname}
+        </h1>
+        
+        <button onClick={this.togglediv}>show</button>
+      </div>
+      <div className='App'>
+        { this.state.show&&<Profile/>}
+      </div>
+      
+      </>
+    )
+  }
+}
+class Profile extends Component{
+  state={
+    profession: "fullstack developer", pishur: "john doe.png", bio:"I love to program and design web pages"
+  }
+  render(){
+    return(
+      <>
+      <div className='App'>
+      <img src={this.state.pishur}/>
+      <h2>{this.state.profession}</h2>
+      <h2>{this.state.bio}</h2>
+      </div>
+      
+      </>
+    )
+  }
 }
 
 export default App;
